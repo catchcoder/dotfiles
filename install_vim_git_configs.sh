49b90f650bd702eb2775048c976a1f777a9dfaf6
Chris Hawkins <catchcoder@gmail.com>
2019-05-20 12:34:26 +0100

    Add Update-alternatives command for vi / vim

--- install_vim_git_configs.sh
+++ install_vim_git_configs.sh
@@ -1,3 +1,8 @@
 cp .vimrc ~
 cp .gitconfig ~
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+sudo update-alternatives --set editor /usr/local/bin/vim
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+sudo update-alternatives --set vi /usr/local/bin/vim
